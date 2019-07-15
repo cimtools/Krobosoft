53292f14639f8472a22e8493f8b4e6f005b29155
GiulianoWF <Giuliano.Floriani@edu.pucrs.br>
2019-07-15 17:25:23 -0300

    Ajuste no método para atualização da lista de COM ports

--- alfa/mainwindow.cpp
+++ alfa/mainwindow.cpp
@@ -3,29 +3,33 @@
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    QList<QSerialPortInfo> infoList = QSerialPortInfo::availablePorts();
-    //foreach(QSerialPortInfo info, infoList) QTextStream(stdout) << info.portName() << "\n";
-
+    ui(new Ui::MainWindow){
     ui->setupUi(this);
-    foreach(QSerialPortInfo info, infoList) ui->comboBox->addItem(info.portName());
-    this->startTimer(200);
+    listaPortasCOM = QSerialPortInfo::availablePorts();
+    foreach(QSerialPortInfo info, listaPortasCOM) ui->comboBox->addItem(info.portName());
+    this->startTimer(200); //Timer usado para checar se houve mudaça na lista de portas COM.
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow(){
     delete ui;
 }
 
-void MainWindow::on_comboBox_activated(const QString &arg1)//quando selecionar uma das opções da lista
-{
-    QTextStream(stdout)<<"ativado\n";
+void MainWindow::on_comboBox_activated(const QString &arg1){ //quando selecionar uma das opções da lista
+    QTextStream(stdout)<<"A porta selecionada eh " << arg1;
 }
 
 void MainWindow::timerEvent(QTimerEvent *a){
-    ui->comboBox->clear();
-    QList<QSerialPortInfo> infoList = QSerialPortInfo::availablePorts();
-    foreach(QSerialPortInfo info, infoList) ui->comboBox->addItem(info.portName());
-
+    //Se a lista atual de portas COM for diferente da armazenada, atualizar a lista tanto na GUI quanto a armazenada na classe.
+    QList<QSerialPortInfo> listaAtualPortas = QSerialPortInfo::availablePorts();
+    if( !std::equal( listaPortasCOM.begin(), listaPortasCOM.end(),
+                    listaAtualPortas.begin(),
+                    []( const QSerialPortInfo & a, const QSerialPortInfo & b ){
+                        return (a.portName() == b.portName())? true: false;
+                    }
+                    ) ) {
+        ui->comboBox->clear();
+        foreach(QSerialPortInfo info, listaAtualPortas) ui->comboBox->addItem(info.portName());
+        listaPortasCOM = listaAtualPortas;
+    }
 }
+
