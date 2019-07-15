244e9bf4066862bb678634c5f29f2b52a1c8e007
GiulianoWF <Giuliano.Floriani@edu.pucrs.br>
2019-07-15 15:24:41 -0300

    Ajuste de arquivos

--- alfa/mainwindow.cpp
+++ alfa/mainwindow.cpp
@@ -5,10 +5,27 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    QList<QSerialPortInfo> infoList = QSerialPortInfo::availablePorts();
+    //foreach(QSerialPortInfo info, infoList) QTextStream(stdout) << info.portName() << "\n";
+
     ui->setupUi(this);
+    foreach(QSerialPortInfo info, infoList) ui->comboBox->addItem(info.portName());
+    this->startTimer(200);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_comboBox_activated(const QString &arg1)//quando selecionar uma das opções da lista
+{
+    QTextStream(stdout)<<"ativado\n";
+}
+
+void MainWindow::timerEvent(QTimerEvent *a){
+    ui->comboBox->clear();
+    QList<QSerialPortInfo> infoList = QSerialPortInfo::availablePorts();
+    foreach(QSerialPortInfo info, infoList) ui->comboBox->addItem(info.portName());
+
 }
