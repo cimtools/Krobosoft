4e819a25e58e0d13770701993e1937ab0c85f9c2
GiulianoWF <Giuka.91@hotmail.com>
2019-07-15 15:24:41 -0300

    Ajuste de arquivos

--- README.md
+++
@@ -1,2 +0,0 @@
-# new_labcim_labBot
-Novo software para programação de robôs Intellitek, com interpretação para ACL. Nome temporário.
--- alfa/mainwindow.h
+++ alfa/mainwindow.h
@@ -2,6 +2,14 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QTableWidget>
+#include <QApplication>
+#include <QTextStream>
+#include <QComboBox>
+#include <QStringList>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +23,13 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_comboBox_activated(const QString &arg1);
+
+    void timerEvent(QTimerEvent *a);
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow * ui;
+    int timerId =0;
 };
 
 #endif // MAINWINDOW_H
