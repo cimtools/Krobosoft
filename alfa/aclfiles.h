50dd25485893a1ed1daf14fe0a6acfbc50b205d0
Gustavo Leal <gustavohleal96@gmail.com>
2019-07-22 13:43:15 -0300

    Model of documentation added

--- alfa/aclfiles.h
+++ alfa/aclfiles.h
@@ -10,6 +10,7 @@
 class aclFiles
 {
 private:
+
     QStringList filenames;
     QString filename;
     QFileDialog *fileDialog;
@@ -18,6 +19,11 @@ public:
     aclFiles();
     void setFilename();
     QString getFilename();
+    /**
+    * @brief Save content of QString into the file pointed by filename
+    * @param content QString The content of the text-edit object to be saved
+    * @return bool Confirms if the saving has occurrely succesfully
+    */
     bool saveToFile(QString content);
     bool openFile();
     bool openFiles();
